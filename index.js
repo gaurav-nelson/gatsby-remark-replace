867b96690640bda8b9b3408747c4629271b35fd3
Gaurav Nelson <gaurav@stackrox.com>
2019-11-03 19:25:00 +1000

    big bang

---
+++ index.js
@@ -0,0 +1,45 @@
+import visit from 'unist-util-visit'
+import escapeStringRegexp from 'escape-string-regexp'
+
+module.exports = ({ markdownAST }, { replacements = {}, prefix = '%' }) => {
+  // Attaches prefix to the start of the string.
+  const attachPrefix = str => (prefix || '') + str
+
+  // Removes prefix from the start of the string.
+  const stripPrefix = str =>
+    prefix ? str.replace(RegExp(`^${prefix}`), '') : str
+
+  // RegExp to find any replacement keys.
+  const regexp = RegExp(
+    '(' +
+      Object.keys(replacements)
+        .map(key => escapeStringRegexp(attachPrefix(key)))
+        .join('|') +
+      ')',
+    'g',
+  )
+
+  const replacer = (_match, name) => replacements[stripPrefix(name)]
+
+  visit(markdownAST, 'text', node => {
+    const processedText = node.value.replace(regexp, replacer)
+    node.value = processedText
+  })
+  
+  visit(markdownAST, 'code', node => {
+    const processedText = node.value.replace(regexp, replacer)
+    node.value = processedText
+  })
+  
+  visit(markdownAST, 'link', node => {
+    const processedText = node.value.replace(regexp, replacer)
+    node.value = processedText
+  })
+  
+  visit(markdownAST, 'inlineCode', node => {
+    const processedText = node.value.replace(regexp, replacer)
+    node.value = processedText
+  })
+  
+  return markdownAST
+}
